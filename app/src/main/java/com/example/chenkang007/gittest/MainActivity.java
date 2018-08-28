ee51fe31123e14a8186776cdc063c0978982ed16
ckndy <chenkang007@ke.com>
2018-08-28 17:20:13 +0800

    调试

--- app/src/main/java/com/example/chenkang007/gittest/MainActivity.java
+++ app/src/main/java/com/example/chenkang007/gittest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chenkang007.gittest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -12,5 +13,11 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     button = findViewById(R.id.bt);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
   }
 }
