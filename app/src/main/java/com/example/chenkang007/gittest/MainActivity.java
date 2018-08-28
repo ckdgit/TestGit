a1912ba65972049dc7dae93046387937c85b40f0
ckndy <chenkang007@ke.com>
2018-08-28 17:08:06 +0800

    feat：点击功能

--- app/src/main/java/com/example/chenkang007/gittest/MainActivity.java
+++ app/src/main/java/com/example/chenkang007/gittest/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.chenkang007.gittest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+  Button button;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    button = findViewById(R.id.bt);
   }
 }
